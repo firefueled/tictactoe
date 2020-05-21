b8faa72cb0d9a1e34c6cda4f2e091df477b68580
firefueled <slpkvol3@gmail.com>
2020-05-21 00:05:16 -0300

    Move utility methods to separate class

--- TicTacSadTester/TestStrategy.cs
+++ TicTacSadTester/TestStrategy.cs
@@ -13,61 +13,14 @@ namespace TicTacSadTester
         public void Strategy_PlaysOnePlay<T>(T strategyType) where T: MatchStrategy
         {
             MatchStrategy strategy = Activator.CreateInstance<T>();
-            var board = BuildBoard(4, 5);
+            var board = TestUtils.BuildBoard(4, 5);
             
-            var availableBoardPlacesBefore = CountAvailableBoardPlaces(board);
+            var availableBoardPlacesBefore = TestUtils.CountAvailableBoardPlaces(board);
             board = strategy.DoPlay(board, Play.X);
-            var availableBoardPlacesAfter = CountAvailableBoardPlaces(board);
+            var availableBoardPlacesAfter = TestUtils.CountAvailableBoardPlaces(board);
             
             Assert.AreEqual(1, availableBoardPlacesBefore - availableBoardPlacesAfter, 
                 "A diferença da quantidade de casas antes e depois nao é -1.");
-        }
-
-        private static int CountAvailableBoardPlaces(List<List<Play>> board)
-        {
-            return 
-                (from row in board
-                    from col in row
-                        where col == Play.Empty
-                        select col).Count();
-        }
-        
-        public static List<List<Play>> BuildBoard(int length, int width)
-        {
-            var board = new List<List<Play>>(length);
-            
-            for (var i = 0; i < length; i++)
-            {   
-                board.Add(new List<Play>(width));
-                var line = board[i];
-                
-                for (var j = 0; j < width; j++)
-                {
-                    line.Add(Play.Empty);
-                }
-            }
-            
-            // Escolhe duas casas para bloquear
-            var rand = new Random();
-            var firstBlocker = new[]
-            {
-                rand.Next(0, length - 1), 
-                rand.Next(0, length - 1)
-            };
-
-            int[] secondBlocker = null;
-            while (secondBlocker == null || secondBlocker.SequenceEqual(firstBlocker)) {
-                secondBlocker = new[]
-                {
-                    rand.Next(0, width - 1), 
-                    rand.Next(0, width - 1)
-                };
-            }
-            
-            // Bloqueia duas casas
-            board[firstBlocker[0]][firstBlocker[1]] = Play.Blocked;
-            board[secondBlocker[0]][secondBlocker[1]] = Play.Blocked;
-            return board;
         }
     }
 }
---
+++ TicTacSadTester/TestUtils.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TicTacSad;
+
+namespace TicTacSadTester
+{
+    public static class TestUtils
+    {
+        public static int CountAvailableBoardPlaces(List<List<Play>> board)
+        {
+            return 
+                (from row in board
+                    from col in row
+                    where col == Play.Empty
+                    select col).Count();
+        }
+
+        public static List<List<Play>> BuildBoard(int length, int width)
+        {
+            var board = new List<List<Play>>(length);
+            
+            for (var i = 0; i < length; i++)
+            {   
+                board.Add(new List<Play>(width));
+                var line = board[i];
+                
+                for (var j = 0; j < width; j++)
+                {
+                    line.Add(Play.Empty);
+                }
+            }
+            
+            // Escolhe duas casas para bloquear
+            var rand = new Random();
+            var firstBlocker = new[]
+            {
+                rand.Next(0, length - 1), 
+                rand.Next(0, length - 1)
+            };
+
+            int[] secondBlocker = null;
+            while (secondBlocker == null || secondBlocker.SequenceEqual(firstBlocker)) {
+                secondBlocker = new[]
+                {
+                    rand.Next(0, width - 1), 
+                    rand.Next(0, width - 1)
+                };
+            }
+            
+            // Bloqueia duas casas
+            board[firstBlocker[0]][firstBlocker[1]] = Play.Blocked;
+            board[secondBlocker[0]][secondBlocker[1]] = Play.Blocked;
+            return board;
+        }
+    }
+}
